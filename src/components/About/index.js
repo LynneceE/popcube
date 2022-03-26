7e4b373c1e340cfbcd5bdb49170d78af2a7006d4
bhari eutsey <bharilynn@outlook.com>
2022-03-25 20:17:39 -0500

    about section complete

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,33 +4,34 @@ import React from 'react';
 function About() {
   return (
     
-      <div class="mycard">
-          
-          <div class="inner__mycard">
-
-              <div class="mycard__face mycard__face--front">
-                  <h2>Click Me!</h2>
-              </div>
-              <div class="mycard__face mycard__face--back">
-                  <div class="mycard__content">
-                      <div class="mycard__header">
-                          <img src="memoji.jpg" alt=""/>
-                      </div>
-                      <div class="mycard__body">
-                          <h3>Greetings</h3>
-                          <p>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                          </p>
-                      </div>
-                  </div>
-              </div>
+      
+                 <div class="card">
+                     <div class="flip-inner-card">
+                         <div class="flip-front">
+                             <h2>Hover Me</h2>
+                         </div>
+                         <div class="flip-back">
+                             <div class="content">
+                                 <div class="header">
+                                 <img src= "./assets/images/prfl-pic.jpg"  alt=""/>
+                             <h2>Hi! I'm Lynnece</h2>
+                             <div class="body">
+                             <p>
+                                 I'm a <span><b>fullstack</b></span> web development student, 
+                                 but backend development stole my heart! I've completed the <span><b>MERN</b></span> stack.
+                                 I was born and raised in Miami, Fl but now live in San Antonio, Tx. I guess city life really isn't for me.
+                                 Feel free to look around, I've linked some of my work in the <span><b>Portfolio</b></span> tab.
+                                 I'm looking forward to working with you!
 
 
-          </div>
-         
-      </div>
+                            </p>
+                             </div>
+                             
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
     
   );
 }
