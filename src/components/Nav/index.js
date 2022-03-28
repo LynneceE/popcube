495659f44b338a1523a9cebd6369f081a42a535d
bhari eutsey <bharilynn@outlook.com>
2022-03-27 22:25:25 -0500

    contact page set to render conditionally but not working

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,24 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 import React from 'react';
 
-function Nav() {
+function Nav(props) {
+    
+    const {
+        portfolioSelected,
+        setPortfolioSelected,
+        contactSelected,
+        setContactSelected,
+        aboutSelected,
+        setAboutselected
+
+    } = props;
+
     return (
         <header>
-            <a href='#' class="myheader">my<span>Portfolio</span></a>
+            <h2 class="myheader">my<span>Portfolio</span></h2>
             <ul>
-                <li> <a href='#about'>About Me</a> </li>
-                <li> <a href='#portfolio'>Portfolio</a> </li>
+                <li> <a href='#about' onClick={() => setContactSelected(false)}>About Me</a></li>
+                <li> <a href='#portfolio' >Portfolio</a> </li>
                 <li> <a href='#contact'>Contact</a> </li>
                 <li> <a href='#resume'>Resume</a> </li>
             </ul>
