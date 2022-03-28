d9e8802bbeb8882acc563a9b40851a7456c9c3d1
bhari eutsey <bharilynn@outlook.com>
2022-03-27 20:36:11 -0500

    contact form complete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Contact></Contact>
         <Portfolio></Portfolio>
         <About></About>
       </main>
